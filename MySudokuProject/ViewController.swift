dbf77de327ddee86727fcc55e56d182e3bb46dc0
Dmitriy Sadyrev <sadyrevdimon@gmail.com>
2022-04-07 21:04:46 +0300

    Initial Commit

---
+++ MySudokuProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySudokuProject
+//
+//  Created by Дмитрий Садырев on 07.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
